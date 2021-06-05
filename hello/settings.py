7769cd8cc7d86cca3e43c8d62552827e1068a1a8
deepak3535 <46932508+deepak3535@users.noreply.github.com>
2021-06-05 13:54:12 +0530

    second commit

--- hello/settings.py
+++ hello/settings.py
@@ -123,6 +123,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATIC_ROOT='staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
